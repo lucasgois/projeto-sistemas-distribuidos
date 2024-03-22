181ff27f26d38220da2874087f0fac3dcbef4837
Lucas Gois <lucas.alves.de.gois@gmail.com>
2024-03-21 21:13:33 -0300

    Adicionado metodo de teste de banco de dados

--- Servidor/src/main/java/com/github/lucasgois/servidor/Main.java
+++ Servidor/src/main/java/com/github/lucasgois/servidor/Main.java
@@ -1,10 +1,17 @@
 package com.github.lucasgois.servidor;
 
+import com.github.lucasgois.servidor.banco.HibernateUtil;
+import com.github.lucasgois.servidor.banco.entidades.Usuario;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.StandardCharsets;
 
 public class Main extends Application {
 
@@ -13,7 +20,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(final Stage primaryStage) throws Exception {
+    public void start(@NotNull final Stage primaryStage) throws Exception {
         final FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/views/tela_principal_servidor.fxml"));
 
@@ -23,6 +30,27 @@ public class Main extends Application {
         primaryStage.setScene(cena);
         primaryStage.setTitle("Titulo da Minha Aplicação");
         primaryStage.show();
+
+
+    }
+
+    private void teste() {
+        //        Conexao.init();
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Usuario entidade = new Usuario();
+        entidade.setNome("Teste");
+        entidade.setSenha("123".getBytes(StandardCharsets.UTF_8));
+        session.save(entidade);
+
+        session.getTransaction().commit();
+        session.close();
+
+        HibernateUtil.shutdown();
+
     }
 
 }
